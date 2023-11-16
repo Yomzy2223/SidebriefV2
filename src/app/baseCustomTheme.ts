1fc2b807382c00af2645efb66c5ffe8b7a8ce4ad
Jesuloba <jesulobajohn@gmail.com>
2023-11-16 20:12:52 +0100

    button variants done

---
+++ src/app/baseCustomTheme.ts
@@ -0,0 +1,15 @@
+import type { ThemeProps } from "flowbite-react";
+
+export const customTheme: ThemeProps = {
+	theme: {
+		button: {
+			color: {
+				primary:
+					"bg-primary text-primary-foreground border border-transparent enabled:hover:bg-primary-dark",
+				magenta:
+					"bg-magenta text-magenta-foreground border border-transparent enabled:hover:bg-magenta-dark",
+				ghost: "bg-transparent text-foreground enabled:hover:bg-foreground/10",
+			},
+		},
+	},
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [require("flowbite/plugin")],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./node_modules/flowbite/**/*.js",
+		"./node_modules/flowbite-react/**/*.js",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: {
+					DEFAULT: "hsl(var(--background))",
+				},
+				foreground: {
+					DEFAULT: "hsl(var(--foreground))",
+					light: "hsl(var(--foreground-light))",
+				},
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+					dark: "hsl(var(--primary-dark))",
+				},
+				magenta: {
+					DEFAULT: "hsl(var(--magenta))",
+					foreground: "hsl(var(--magenta-foreground))",
+					dark: "hsl(var(--magenta-dark))",
+				},
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
 };
 export default config;
