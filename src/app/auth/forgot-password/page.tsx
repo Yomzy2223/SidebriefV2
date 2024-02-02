72a44bfc7bd26b3111a3147051edeb91723d3c87
yomzy2223 <oluwolesayo10@gmail.com>
2024-02-02 10:39:29 +0100

    Updated auth configuration

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -8,60 +8,53 @@ import { ArrowRightCircle } from "lucide-react";
 import { useForgotPassword } from "@/services/auth";
 
 export default function ForgotPassword() {
-	const forgotPassword = useForgotPassword();
+  const forgotPassword = useForgotPassword();
 
-	const handleForgot = async (values: { email: string }) => {
-		forgotPassword.mutate(
-			{ email: values.email },
-			{
-				onError: (err: any) => {
-					console.log(err.response.data.error);
-				},
-				onSuccess: (data) => {
-					console.log(data);
-				},
-			}
-		);
-	};
+  const handleForgot = async (values: { email: string }) => {
+    forgotPassword.mutate(
+      { email: values.email },
+      {
+        onError: (err: any) => {
+          console.log(err.response.data.error);
+        },
+        onSuccess: (data) => {
+          console.log(data);
+        },
+      }
+    );
+  };
 
-	return (
-		<AuthFormWrapper textTitle="Forgot Password">
-			<DynamicForm
-				formInfo={formInfo}
-				defaultValues={defaultValues}
-				formSchema={forgotSchema}
-				onFormSubmit={handleForgot}
-			>
-				<Button
-					type="submit"
-					color="secondary"
-					isProcessing={forgotPassword.isPending}
-				>
-					Get reset link <ArrowRightCircle className="ml-1" />
-				</Button>
-			</DynamicForm>
-		</AuthFormWrapper>
-	);
+  return (
+    <AuthFormWrapper title="Forgot Password">
+      <DynamicForm
+        formInfo={formInfo}
+        defaultValues={defaultValues}
+        formSchema={forgotSchema}
+        onFormSubmit={handleForgot}
+      >
+        <Button type="submit" color="secondary" isProcessing={forgotPassword.isPending}>
+          Get reset link <ArrowRightCircle className="ml-1" />
+        </Button>
+      </DynamicForm>
+    </AuthFormWrapper>
+  );
 }
 
 const formInfo = [
-	{
-		name: "email",
-		label: "Enter Email Address",
-		type: "email",
-		textInputProp: {
-			placeholder: "Enter your email address",
-		},
-	},
+  {
+    name: "email",
+    label: "Enter Email Address",
+    type: "email",
+    textInputProp: {
+      placeholder: "Enter your email address",
+    },
+  },
 ];
 
 const forgotSchema = z.object({
-	email: z
-		.string()
-		.email("Enter a valid email")
-		.min(1, { message: "Enter your email address" }),
+  email: z.string().email("Enter a valid email").min(1, { message: "Enter your email address" }),
 });
 
 const defaultValues = {
-	email: "",
+  email: "",
 };
