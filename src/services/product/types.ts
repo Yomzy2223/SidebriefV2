198c6c3668cbe0ae6224a7e8a915db3209b228ea
jesuloba <jesulobajohn@gmail.com>
2024-02-28 11:59:56 +0100

    page 2 persistence done

--- src/services/product/types.ts
+++ src/services/product/types.ts
@@ -1,83 +1,91 @@
-import type {
-	serviceProductType,
-	serviceFormSubFormType,
-} from "../service/types";
+import type { serviceProductType } from "../service/types";
 
 export type productType = {
-	id: string;
-	email: string | null;
-	address: string | null;
-	paid: boolean;
-	completed: boolean;
-	currentState: "START";
-	status: "pending";
-	createdAt: string;
-	updatedAt: string;
-	productId: string | null;
-	userId: string;
-	productQA: productQAType[];
-	product: serviceProductType;
+  id: string;
+  email: string | null;
+  address: string | null;
+  paid: boolean;
+  completed: boolean;
+  currentState: "START";
+  status: "pending";
+  createdAt: string;
+  updatedAt: string;
+  productId: string | null;
+  userId: string;
+  productQA: productQAType[];
+  product: serviceProductType;
 };
 
 export type File = {
-	name: string;
-	description?: string;
-	link: string;
-	type: string;
+  name: string;
+  description?: string;
+  link: string;
+  type: string;
 };
 
 export type SubFormProfile = {
-	question: string;
-	answer: string[];
-	type: string;
-	compulsory: boolean;
+  question: string;
+  answer: string[];
+  type: string;
+  compulsory: boolean;
 };
 
 export type FormItem = {
-	question: string;
-	answer: string[];
-	type: string;
-	compulsory: boolean;
-	isGeneral: boolean;
-	// subForm: boolean;
-	// profile: SubFormProfile[];
-	file?: File;
+  question: string;
+  answer: string[];
+  type: string;
+  compulsory: boolean;
+  isGeneral: boolean;
+  // subForm: boolean;
+  // profile: SubFormProfile[];
+  file?: File;
 };
 
 export type saveProductQAPayload = {
-	productId: string;
-	form: {
-		title: string;
-		description: string;
-		type: string;
-		compulsory: boolean;
-		isGeneral: boolean;
-		subForm: FormItem[];
-	};
+  productId: string;
+  form: {
+    title: string;
+    description: string;
+    type: string;
+    compulsory: boolean;
+    isGeneral: boolean;
+    subForm: FormItem[];
+  };
 };
 
 export type productQAType = {
-	id: string;
-	question: string;
-	answer: string[];
-	type: string;
-	compulsory: boolean;
-	isGeneral: boolean;
-	fileName: string | null;
-	fileDescription: string | null;
-	fileType: string | null;
-	fileLink: string | null;
-	createdAt: string;
-	updatedAt: string;
-	productId: string;
+  compulsory: boolean;
+  createdAt: string;
+  description: string;
+  id: string;
+  isDeprecated: boolean;
+  isGeneral: boolean;
+  requestId: string;
+  subForm: SubformQAType[];
+  title: string;
+  type: string;
+  updatedAt: string;
 };
 
 export type addServiceToProductPayload = {
-	serviceId: string;
-	productId: string;
+  serviceId: string;
+  productId: string;
 };
 
 export type createProductPayload = {
-	userId: string;
-	productId: string;
+  userId: string;
+  productId: string;
+};
+
+export type SubformQAType = {
+  answer: string[];
+  compulsory: boolean;
+  fileLink: string | null;
+  fileName: string | null;
+  fileType: string | null;
+  id: string;
+  //   isDeprecated: boolean;
+  question: string;
+  requestQAId: string;
+  type: string;
 };
