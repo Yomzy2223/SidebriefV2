328bc9c52d209baf8606cc52583f85b0f40b0394
Damola Olutoke <olutokeezekiel18@gmail.com>
2024-05-10 12:03:41 +0100

    add new onboard ui screens

--- src/app/(mainpages)/onboard/page.tsx
+++ src/app/(mainpages)/onboard/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import OnboardIntro from "./(steps)/intro/page";
 
 const Onboard = () => {
-  return <div>Onboard</div>;
+  return (
+    <div>
+        <OnboardIntro/>
+    </div>
+  )
 };
 
 export default Onboard;
